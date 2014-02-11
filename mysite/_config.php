627cb9597888ebd61dc6e4486413dbb80d019198
Dustin Quam <quamsta@gmail.com>
2014-02-11 14:38:37 -0600

    converted to a vagrant project

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,27 +3,11 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'localhost', 
-	"username" => 'root', 
-	"password" => 'root', 
-	"database" => 'uiesw',
-	"path" => '',
-);
-
-MySQLDatabase::set_connection_charset('utf8');
-
-// This line set's the current theme. More themes can be
-// downloaded from http://www.silverstripe.org/themes/
-SSViewer::set_theme('esw');
+global $database;
+$database = 'usgbc';
+ 
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
 
 // Set the site locale
 i18n::set_locale('en_US');
-
-// enable nested URLs for this site (e.g. page/sub-page/)
-SiteTree::enable_nested_urls();
-
-Deprecation::notification_version('3.1.0-dev');
-Security::setDefaultAdmin('admin','password');
